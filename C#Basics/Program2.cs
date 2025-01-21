88d259d24adc9f7492001dc16a7dc99e1d844382
Srivaths <srivaths.iyer@mitaoe.ac.in>
2025-01-21 12:11:07 +0530

    GitSetup

---
+++ C#Basics/Program2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Program 2");
+        }
+    }
+}
